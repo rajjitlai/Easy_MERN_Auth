a2a88a4ba156d54a830c91cdfe5f8032934c76a9
RAJJIT <2.0lairaj@gmail.com>
2024-12-11 21:47:48 +0530

    code-1.5-authController-code

---
+++ server/controllers/authController.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcryptjs"
+import { JsonWebToken } from "jsonwebtoken"
+import userModel from "../models/userModel";
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.json({ success: false, message: 'Missing Details' })
+    }
+
+    try {
+        const existingUser = await userModel.findOne({ email })
+        if (existingUser) { return res.json({ success: false, message: 'User already exists' }) }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = new userModel({ name, email, password: hashedPassword })
+
+        await user.save()
+
+        const token = jwt.sign({ id: user._id })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
